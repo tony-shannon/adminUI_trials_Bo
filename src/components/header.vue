6c86021ef550809bf92bda56876645fa0b31d333
Tony Shannon <tony.shannon@ripple.foundation>
2020-01-24 21:05:28 +0000

    Add files via upload

---
+++ src/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="header">
+      <header class="navbar navbar-inverse  bs-docs-nav" role="banner">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand">Web Component/Vue Admin UI Demo</a>
+            </div>
+        </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'This is header'
+    }
+  }
+}
+</script>
+
+<style>
+header {
+  position: fixed !important;
+  width: 100%;
+  z-index: 2000;
+}
+.navbar{
+    background: #333;
+}
+.container{
+    margin: 0px 100px;
+}
+.navbar-brand {
+    font-size: 30px;
+}
+</style>
